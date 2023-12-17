d2eeaa69681e4cb113cd4f270ea2c732be02f91a
nvll <nvll.zen@gmail.com>
2023-12-17 08:43:29 +0700

    updated config of i3 and alacritty

--- autostart-scripts/i3-start.sh
+++ autostart-scripts/i3-start.sh
@@ -1,3 +1,4 @@
 xinput set-prop 10 "libinput Tapping Enabled" 1
 feh --bg-scale ~/Documents/cp2077-linux/cp2077nightcitydark.png
+/home/nvll/Documents/cp2077-linux/autostart-scripts/polybar.sh
 /usr/lib/polkit-kde-authentication-agent-1
